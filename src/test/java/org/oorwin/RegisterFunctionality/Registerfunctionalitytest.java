819d12f9a66f719e76e790c5c8d554043b4bf9c4
Srikanth Akkala <srikanthakkala0@gmail.com>
2025-11-16 09:41:17 +0530

    Testt

--- src/test/java/org/oorwin/RegisterFunctionality/Registerfunctionalitytest.java
+++ src/test/java/org/oorwin/RegisterFunctionality/Registerfunctionalitytest.java
@@ -11,7 +11,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RegisterfunctionalityTest {
 	@Test
-	public void registerfunctionalityTestt() {
+	public void RegisterFunctionalityTestt() {
 		 WebDriverManager.chromedriver().setup();
 		 WebDriver driver = new ChromeDriver();
 
